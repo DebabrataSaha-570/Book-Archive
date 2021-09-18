4ddc269f4c58195e410d161b649f2b31177c0b18
Debabrata Saha <sahadebabrata570@gmail.com>
2021-09-18 23:10:20 +0600

    display book data added and data showed on the website

--- js/script.js
+++ js/script.js
@@ -14,8 +14,28 @@ const loadBookData = () => {
     const url = `https://openlibrary.org/search.json?q=${searchResult}`
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.docs))
+        .then(data => displayBookData(data.docs))
 
 }
 
-
+const displayBookData = (books) => {
+    console.log(books)
+    books.forEach(book => {
+        console.log(book)
+        const bookContainer = document.getElementById('books-container')
+        const div = document.createElement('div')
+        div.classList.add('col')
+        div.innerHTML =
+            `
+        <div class="card h-100" >
+                    <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top card-image" alt="image..">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+        `
+        bookContainer.appendChild(div)
+    })
+}
