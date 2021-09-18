dcabf6aea8ff83e5575308cc57a09be7b1ec2a8f
Debabrata Saha <sahadebabrata570@gmail.com>
2021-09-19 01:29:15 +0600

    some error handling added

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 const bookContainer = document.getElementById('books-container')
+const searchResultContainer = document.getElementById('search-result-container')
+const noResultContainer = document.getElementById('no-result')
 
 // get search value from input field 
 
 const getInputValue = () => {
     const searchField = document.getElementById('search-field')
     const searchValue = searchField.value;
+    console.log('search value', searchValue.length)
 
     searchField.value = '';
     return searchValue;
+
 }
 
 // load data from api 
@@ -22,18 +26,40 @@ const loadBookData = () => {
     </div>
     </div>
     `
-    const url = `https://openlibrary.org/search.json?q=${searchResult}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayBookData(data.docs))
+    searchResultContainer.innerHTML = '';
+    noResultContainer.innerHTML = '';
+
+    if (searchResult.length > 0) {
+        const url = `https://openlibrary.org/search.json?q=${searchResult}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayBookData(data.docs))
+            .catch(err => console.log(err))
+    }
+    else {
+        alert('please write something to display')
+        bookContainer.innerHTML = '';
+    }
 
 }
 
 // display data to website 
 const displayBookData = (books) => {
-    console.log(books)
+
+    if (books.length === 0) {
+        noResultContainer.innerHTML = `
+        <p> No result found </p>
+        `
+    }
+
+
+    searchResultContainer.innerHTML = `
+    <p> showing first ${books.length / 2} items of ${books.length} </p>
+    `
+
+    console.log(books.length, books)
     bookContainer.textContent = '';
-    books.forEach(book => {
+    books.slice(0, `${books.length / 2}`).forEach(book => {
         const div = document.createElement('div')
         div.classList.add('col')
         div.innerHTML =
@@ -42,8 +68,9 @@ const displayBookData = (books) => {
                     <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top card-image " alt="image..">
                     <div class="card-body">
                         <h5 class="card-title text-center">${book.title}</h5>
-                        <p class="card-text"> <span> Author name: </span> ${book.author_name}</p>
-                        <p class="card-text"> <span> First Publish year: </span> ${book.first_publish_year}</p>
+
+                        <p class="card-text"> <span> Author name: </span> ${book.author_name ? book.author_name : 'Not available'}</p>
+                        <p class="card-text"> <span> First Publish year: </span> ${book.first_publish_year ? book.first_publish_year : 'Not available'}</p>
                     </div>
                 </div>
         `
